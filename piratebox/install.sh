1346ce0d2613b1532c1aa1db80d9b3b6009f1ee8
Brannon Dorsey <brannon@brannondorsey.com>
2015-02-22 14:08:22 -0600

    Add optional addon packages to install.sh

--- piratebox/install.sh
+++ piratebox/install.sh
@@ -87,33 +87,68 @@ fi
 #TODO missing anything in $DEPENDENCIES?
 # Modified Script by martedì at http://www.mirkopagliai.it/bash-scripting-check-for-and-install-missing-dependencies/
 PKGSTOINSTALL="hostapd lighttpd dnsmasq"
+EXTRAPKGSTOINSTALL="php5-cgi"
 
 # If some dependencies are missing, asks if user wants to install
 if [ "$PKGSTOINSTALL" != "" ]; then
-	echo -n "Some dependencies may missing. Would you like to install them? (Y/n): "
-	read SURE
+	
+	echo -n "Piratebox requires the following dependencies:"
+	echo -n "	$PKGSTOINSTALL"
+	echo -n "Would you like to install them now? (Y/n):"
+	read PKGSURE
+
+	if [ "$EXTRAPKGSTOINSTALL" != "" ];
+		echo -n "Piratebox supports the following addon dependencies:"
+		echo -n "	$EXTRAPKGSTOINSTALL"
+		echo -n "Would you like to install them now? (Y/n):"
+		read EXTRAPKGSURE
+	fi
+
 	# If user want to install missing dependencies
-	if [[ $SURE = "Y" || $SURE = "y" || $SURE = "" ]]; then
+	if [[ $PKGSURE = "Y" || $PKGSURE = "y" || $PKGSURE = "" || $EXTRAPKGSURE = "Y" || $EXTRAPKGSURE = "y" || $EXTRAPKGSURE = ""]]; then
 		# Debian, Ubuntu and derivatives (with apt-get)
 		if which apt-get &> /dev/null; then
-			apt-get install $PKGSTOINSTALL
+			if [[ $PKGSURE = "Y" || $PKGSURE = "y" || $PKGSURE = "" ]]; then 
+				apt-get install $PKGSTOINSTALL
+			fi
+			if [[ $EXTRAPKGSURE = "Y" || $EXTRAPKGSURE = "y" || $EXTRAPKGSURE = "" ]]; then
+				apt-get install $PKGSTOINSTALL
+			fi
 		# OpenSuse (with zypper)
 		#elif which zypper &> /dev/null; then
 		#	zypper in $PKGSTOINSTALL
 		# Mandriva (with urpmi)
 		elif which urpmi &> /dev/null; then
-			urpmi $PKGSTOINSTALL
+			if [[ $PKGSURE = "Y" || $PKGSURE = "y" || $PKGSURE = "" ]]; then 
+				urpmi $PKGSTOINSTALL
+			fi
+			if [[ $EXTRAPKGSURE = "Y" || $EXTRAPKGSURE = "y" || $EXTRAPKGSURE = "" ]]; then
+				urpmi $PKGSTOINSTALL
+			fi
 		# Fedora and CentOS (with yum)
 		elif which yum &> /dev/null; then
-			yum install $PKGSTOINSTALL
+			if [[ $PKGSURE = "Y" || $PKGSURE = "y" || $PKGSURE = "" ]]; then 
+				yum install $PKGSTOINSTALL
+			fi
+			if [[ $EXTRAPKGSURE = "Y" || $EXTRAPKGSURE = "y" || $EXTRAPKGSURE = "" ]]; then
+				yum install $PKGSTOINSTALL
+			fi
 		# ArchLinux (with pacman)
 		elif which pacman &> /dev/null; then
-			pacman -Sy $PKGSTOINSTALL
+			if [[ $PKGSURE = "Y" || $PKGSURE = "y" || $PKGSURE = "" ]]; then 
+				pacman -Sy $PKGSTOINSTALL
+			fi
+			if [[ $EXTRAPKGSURE = "Y" || $EXTRAPKGSURE = "y" || $EXTRAPKGSURE = "" ]]; then
+				pacman -Sy $PKGSTOINSTALL
+			fi
 		# Else, if no package manager has been found
 		else
 			# Set $NOPKGMANAGER
 			NOPKGMANAGER=TRUE
-			echo "ERROR: No package manager found. Please, manually install: $PKGSTOINSTALL."
+			echo "ERROR: No package manager found. Please, manually install:"
+			echo "	$PKGSTOINSTALL"
+			echo "and (optional):"
+			echo "	$EXTRAPKGSTOINSTALL"
 		fi
 	fi
 fi
