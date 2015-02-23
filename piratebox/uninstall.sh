7fbbdd6942a34bc470e77747cfdbd2074f63a6be
Brannon Dorsey <brannon@brannondorsey.com>
2015-02-22 18:50:49 -0600

    Update uninstall.sh

--- piratebox/uninstall.sh
+++ piratebox/uninstall.sh
@@ -1,4 +1,7 @@
+echo "Stopping piratebox..."
 sudo /etc/init.d/piratebox stop
+echo "Removing piratebox daemon..."
 sudo update-rc.d piratebox remove 
 sudo rm -v /etc/init.d/piratebox
+echo "Removing piratebox folder..."
 sudo rm -rf -v /opt/piratebox 
